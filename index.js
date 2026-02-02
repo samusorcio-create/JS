18d1149917823b5f62ff9947ddef0cea5d50a1ae
samusorcio-create <samu.sorcio@gmail.com>
2026-02-02 10:02:23 +0100

    up_date function

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("hello you");
+
+function saluta(){
+    console.log("hello you");
+}
+
+saluta();
+saluta();
+saluta();
+
+function salutaUtente(nome_utente){
+    console.log(`caio ${nome_utente}`);
+}
+
+salutaUtente("scimmia");
+salutaUtente("scimpaze");
+salutaUtente("orango");
+salutaUtente("gorilla");
+
+salutaUtente();
+
+function somma(n1, n2){
+    return n1 + n2;
+}
