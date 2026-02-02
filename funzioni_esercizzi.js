cdd52272ea0331b94c0fd522b4774b845fc6ea5b
samusorcio-create <samu.sorcio@gmail.com>
2026-02-02 11:40:19 +0100

    esercizzi 50.40 e 50.50

--- funzioni_esercizzi.js
+++ funzioni_esercizzi.js
@@ -19,4 +19,16 @@ let areaRetangolo = (base, altezza) => base * altezza;
 console.log(areaRetangolo(12,3));
 console.log(areaRetangolo(15,10));
 
+let calcolaQuadrato = (val) => val * val;
+
+console.log(calcolaQuadrato(5));
+console.log(calcolaQuadrato(12));
+console.log(calcolaQuadrato(20));
+
+let celsiusToFahrenheit = (celsius) => (celsius * 9/10) + 32;
+
+console.log(celsiusToFahrenheit(0));
+console.log(celsiusToFahrenheit(25));
+console.log(celsiusToFahrenheit(100));
+
 
