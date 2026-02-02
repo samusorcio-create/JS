f10afa9f04258935479a826ad8388446fe85958e
samusorcio-create <samu.sorcio@gmail.com>
2026-02-02 10:17:45 +0100

    up_date 0.1 function

--- index.js
+++ index.js
@@ -22,3 +22,21 @@ salutaUtente();
 function somma(n1, n2){
     return n1 + n2;
 }
+
+const somma1 = somma(1,6);
+console.log(somma1);
+console.log(somma(10,6));
+
+
+function sotrazione(n1, n2){
+    return n1 - n2;
+}
+
+const somma2 = sotrazione(1,6);
+console.log(somma2);
+console.log(sotrazione(10,6));
+
+function salutaPredefinito(utente, tipo_saluto = "ciao"){
+    console.log(`${tipo_saluto}`,`${utente}`);
+}
+// variante saluto utente
