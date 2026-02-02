ea82b5a91cd57453a6d81d5ec5d9dffee9d7de21
samusorcio-create <samu.sorcio@gmail.com>
2026-02-02 11:35:08 +0100

    esercizio 50.30

--- funzioni_esercizzi.js
+++ funzioni_esercizzi.js
@@ -2,16 +2,21 @@ function saluta(){
     console.log("benvenuto al corso di javascript!")
 }
 
-saluta()
-saluta()
-saluta()
+saluta();
+saluta();
+saluta();
 
 function salutaStudente(nome){
     console.log(`caio ${nome}, buono studio!`);
 }
 
-salutaStudente("no")
-salutaStudente("you")
-salutaStudente("paw")
+salutaStudente("no");
+salutaStudente("you");
+salutaStudente("paw");
+
+let areaRetangolo = (base, altezza) => base * altezza;
+
+console.log(areaRetangolo(12,3));
+console.log(areaRetangolo(15,10));
 
 
